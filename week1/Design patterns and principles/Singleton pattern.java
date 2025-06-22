185e8a08e404d4d67126e8449c9c7156d2b01850
venureddy1234 <ratnavenumadhavareddy@gmail.com>
2025-06-22 18:49:48 +0530

    Update Singleton pattern.java

--- week1/Design patterns and principles/Singleton pattern.java
+++ week1/Design patterns and principles/Singleton pattern.java
@@ -29,3 +29,10 @@ class Main {
         }
     }
 }
+
+//OUTPUT:
+
+Logger Initialized
+Log: This is the first log message
+Log: This is the second log message
+Both logger1 and logger2 are the same instance
