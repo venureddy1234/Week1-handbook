e39407fce1cf39cc9c131c6b4d47e382b894a7eb
venureddy1234 <ratnavenumadhavareddy@gmail.com>
2025-06-22 18:28:05 +0530

    Update Singleton pattern.java

--- Design patterns and principles/Singleton pattern.java
+++ Design patterns and principles/Singleton pattern.java
@@ -33,6 +33,7 @@ class Main {
 
 //OUTPUT:
 
-Opening Word document...
-Opening PDF document...
-Opening Excel document...
+Logger Initialized
+Log: This is the first log message
+Log: This is the second log message
+Both logger1 and logger2 are the same instance
